cea29b6845be7105a316acbee726c2ba4ad653e0
suyogdw11 <suyog.deo11@gmail.com>
2022-10-09 19:15:04 +0530

    Add project files.

---
+++ MySqlApp/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySqlApp.Models;
+using MySqlApp.Services;
+
+namespace MySqlApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> Products;
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            Products= productService.GetProducts();
+
+        }
+    }
+}
